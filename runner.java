de492cb6af6a77f33e60b4895e8aff9f7b072f40
VARUN CHADHA <vchadha99@gmail.com>
2019-11-09 11:38:11 +0530

    Runner Class Added

--- runner.java
+++ runner.java
@@ -1,40 +1,19 @@
-import java.util.*;
-
 public class runner
 {
     public static void main(String []agrs)
     {
-        BinarySearchTree tree = new BinarySearchTree();
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the number of nodes");
-        int num = sc.nextInt();
-        System.out.println("Enter nodes");
-        /*Enter the data nodes in PreOrder or just enter root node first*/
-        for(int i =0 ; i<num ; i++)
-        {
-            tree.insert(sc.nextInt());
-        }
-        //Prints height of tree in terms of edges
-        System.out.println("Edge height " + (tree.height()-1));
-        //Prints level order transversal
-        System.out.println("Level order");
-        tree.level();
-        //Prints reverse of level order transversal
-        System.out.println("\nReverse level order");
-        tree.revlevel();
-        //Prints level order transversal with data aligned right to left
-        System.out.println("\nData left to right in level order");
-        tree.rtllevel();
-        //Prints tree levels spirally
-        System.out.println("\nSpiral printing");
-        tree.spiral();
-        //Prints preorder transversal
-        System.out.println("\nPrints in order");
-        tree.preorder();
-        //Prints postorder transversal
-        System.out.println("\nPrints post order");
-        tree.postorder();
-        sc.close();
-        
+        QueueUsingStack q = new QueueUsingStack();
+        q.enqueue(10);
+        q.enqueue(20);
+        q.enqueue(30);
+        q.enqueue(40);
+        q.enqueue(50);
+        q.enqueue(60);
+        q.show();
+
+        System.out.println("\n"+q.dequeue());
+        System.out.println(q.dequeue()+"\n");
+        q.show();
+       
     }
 }
